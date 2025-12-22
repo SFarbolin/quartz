cd4b2351dee90279715591278559ccad276a4849
SFarbolin <SFarbolin@GMail.com>
2025-12-21 23:53:50 -0800

    Update index.ts

--- quartz/plugins/index.ts
+++ quartz/plugins/index.ts
@@ -45,7 +45,7 @@ export function getStaticResourcesFromPlugins(ctx: BuildCtx) {
 export * from "./transformers"
 export * from "./filters"
 export * from "./emitters"
-export { ClickableImages } from "./clickableImages"
+export { ClickableImages } from "./transformers/clickableImages.ts"
 
 declare module "vfile" {
   // inserted in processors.ts
