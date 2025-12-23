73fa7c1a5a5c4dc89882f53ff98a4b1b03b934da
SFarbolin <SFarbolin@GMail.com>
2025-12-23 15:36:08 -0800

    Hacky force darkMode

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,7 +28,7 @@ export const defaultContentPageLayout: PageLayout = {
           Component: Component.Search(),
           grow: true,
         },
-        { Component: Component.Darkmode() },
+//        { Component: Component.Darkmode() },
       ],
     }),
 //    Component.Explorer(),
@@ -59,7 +59,7 @@ export const defaultListPageLayout: PageLayout = {
           Component: Component.Search(),
           grow: true,
         },
-        { Component: Component.Darkmode() },
+//        { Component: Component.Darkmode() },
       ],
     }),
      Component.DesktopOnly(
